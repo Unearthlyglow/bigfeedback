28df143798a63356137f3eadc6898c5f957d0701
Award <awardmalisi@gmail.com>
2023-11-18 15:08:53 -0600

    working on updating branches

--- cmd/main.go
+++ cmd/main.go
@@ -13,10 +13,9 @@ func main() {
 
 	fmt.Println(word)
 
-fmt.Println("I am what I am")
-=======
+	fmt.Println("I am what I am")
+
 	fmt.Println("For testHome branch, going back to")
 	fmt.Println("Even More testing of the testbrancfffh")
-
 
 }
